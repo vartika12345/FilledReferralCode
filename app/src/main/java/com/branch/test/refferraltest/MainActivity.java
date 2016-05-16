25cf036dcaa51afeda3115786dfe5df251e2fa4a
VARTIKA SHARMA <vartika.1403@gmail.com>
2016-05-17 04:28:29 +0530

    initial commit

--- app/src/main/java/com/branch/test/refferraltest/MainActivity.java
+++ app/src/main/java/com/branch/test/refferraltest/MainActivity.java
@@ -1,10 +1,8 @@
 package com.branch.test.refferraltest;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,19 +12,13 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Iterator;
-import java.util.logging.Logger;
-
 import io.branch.referral.Branch;
 import io.branch.referral.BranchError;
 
 public class MainActivity extends AppCompatActivity {
 
     Branch branch = Branch.getInstance();
-    SharedPreferences sharedpreferences;
     String key;
-    public static final String MY_PREFS_NAME = "MyPrefsFile";
-   // Button btnShare;
     Button btnShareEarn;
     TextView txtReferral;
     TextView txtCode;
@@ -35,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.share_earn);
-
-       // final TextView textView = (TextView) findViewById(R.id.text);
-     //   btnShare = (Button)findViewById(R.id.btnShare);
         btnShareEarn = (Button)findViewById(R.id.btnShareEarn);
         txtReferral = (TextView)findViewById(R.id.txtReferral);
         txtCode = (TextView)findViewById(R.id.txtCode);
@@ -46,10 +35,9 @@ public class MainActivity extends AppCompatActivity {
         btnShareEarn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent shareIntent = new Intent(Intent.ACTION_VIEW);
-               // shareIntent.setType("text/plain");
-                  shareIntent.setData(Uri.parse("https://bnc.lt/referralcode"));
-                //shareIntent.putExtra(android.content.Intent.EXTRA_TEXT,Uri.parse("https://bnc.lt/referralcode"));
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "https://bnc.lt/referralcode");
                 startActivity(Intent.createChooser(shareIntent, "Share link using"));
             }
         });
@@ -58,17 +46,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onInitFinished(JSONObject referringParams, BranchError error) {
                 if (error == null) {
-                    Toast.makeText(MainActivity.this,"got params ", Toast.LENGTH_LONG).show();
+
                     Log.i("ReferringParams" ,referringParams.toString());
                     try {
                         key = referringParams.getString("key");
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    txtReferral.setText("Referral code : " + key);
-
-                   // txtCode.setVisibility(View.GONE);
-                   // btnShareEarn.setVisibility(View.GONE);
 
                 } else {
                     Log.i("MyApp", error.getMessage());
@@ -81,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Lifecycle callback method
     }
 
     @Override
